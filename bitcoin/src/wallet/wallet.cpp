d40ab0c2e7c83a286e497fdda205b4263791e381
Dinesh Kumar Sharma <dinesh@xiornis.com>
2019-07-16 12:05:45 +0530

    update

--- bitcoin/src/wallet/wallet.cpp
+++ bitcoin/src/wallet/wallet.cpp
@@ -2835,7 +2835,7 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
                 const uint32_t nSequence = coin_control.signalRbf ? MAX_BIP125_RBF_SEQUENCE : (CTxIn::SEQUENCE_FINAL - 1);
                 LogPrintf("Create transaction vin  line 2836 %d", nSequence);
                 for (const auto& coin : setCoins)
-                    LogPrintf("Create transaction vin line 2838 %s", coin.outpoint,CScript());
+                    LogPrintf("Create transaction vin line 2838 %s", coin.outpoint,CScript().ToString());
                     txNew.vin.push_back(CTxIn(coin.outpoint,CScript(),
                                               nSequence));
 
